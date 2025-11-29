5c0f63903176063c0380e597fa653d17529f1dde
AlexJr22 <alexjr2oficial@gmail.com>
2025-11-29 08:58:58 -0300

    add surname property in user class

--- FinanceControl.Domain/Entities/User.cs
+++ FinanceControl.Domain/Entities/User.cs
@@ -1,9 +1,10 @@
 ﻿namespace FinanceControl.Domain.Entities;
 
-public class User(Guid id, string name, string email, string passward)
+public class User(Guid id, string name, string surname,string email, string passward)
 {
     public Guid Id { get; set; } = id;
     public string Name { get; set; } = name;
+    public string Surname { get; set; } = surname;
     public string Email { get; set; } = email;
     public string Password { get; set; } = passward;
 }
